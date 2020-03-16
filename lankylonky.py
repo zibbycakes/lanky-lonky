970d2f067bd213c7178a11d7ee67bd669fa744ef
Allyson Walpole <zibbycakes@gmail.com>
2020-03-16 11:54:15 -0500

    refactor, remove comments, put table name to env file

--- lankylonky.py
+++ lankylonky.py
@@ -12,31 +12,14 @@ from datetime import datetime
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
+VOTE_TABLE = os.getenv('VOTE_TABLE')
 
+print(VOTE_TABLE)
 valid_votes = []
 
 bot = commands.Bot(command_prefix='!')
 dynamodb = boto3.resource('dynamodb', region_name='us-west-2', endpoint_url=os.getenv('ENDPOINT_URL'))
-table = dynamodb.Table('Votes')
-
-# @bot.command(name='start_game', help='start the voting game, setting up who is valid to vote for.')
-# @commands.has_role('GM')
-# async def start(ctx):
-#     global valid_votes
-#     for guild in bot.guilds:
-#         if guild.name == GUILD:
-#             for role in guild.roles:
-#                 if role.name == 'player':
-#                     for member in role.members:
-#                         print(member.nick)
-#                         # may need to refactor to allow for members without nicknames
-#                         if(member.nick != None):
-#                             valid_votes.append(member.nick)
-#                     break
-#             break
-#     print(valid_votes)
-#     await ctx.send('added [' + ', '.join(valid_votes) + '] to the game. Let\'s get started!')
-
+vote_table = dynamodb.Table(VOTE_TABLE)
 
 @bot.command(name='start_game', help='start the voting game, setting up who is valid to vote for.')
 @commands.has_role('GM')
@@ -58,7 +41,7 @@ async def start(ctx, role: discord.Role):
 @commands.has_role('player')
 async def vote(ctx, player:discord.Member):
     print(player.nick)
-    item = table.put_item(
+    item = vote_table.put_item(
         Item={
             'VotedPlayer':player.nick,
             'Timestamp':datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"),
@@ -69,17 +52,5 @@ async def vote(ctx, player:discord.Member):
     print(json.dumps(item, indent=4))
     # need to catch error if member name is not correct
     # need to catch error for wrong role type
-
-@bot.command(name='roll_dice', help='Simulates rolling dice.')
-async def roll(ctx, number_of_dice: int, number_of_sides: int):
-    dice = [
-        str(random.choice(range(1, number_of_sides + 1)))
-        for _ in range(number_of_dice)
-    ]
-    await ctx.send(', '.join(dice))
-
-@bot.command(name='hello')
-async def hello(ctx):
-    await ctx.send('hello world')
 
 bot.run(TOKEN)
