d108575d23491d0e1914755b5f72dfff9dccd20d
Allyson Walpole <zibbycakes@gmail.com>
2020-04-04 17:15:37 -0500

    tally votes on day end. validate voters on day start

--- lankylonky.py
+++ lankylonky.py
@@ -25,6 +25,7 @@ day_counter = 0
 game_started = False
 daytime = False
 current_game_name = ''
+role_for_valid_voters = None
 
 @bot.command(name='start_game', help='Start listening for votes. This sets up who is valid to vote for by using the provided role. Needs a unique game name identifier with no spaces, and the role being used by the game players.')
 @commands.has_role('GM')
@@ -35,16 +36,13 @@ async def start(ctx, game_name, role: discord.Role): #verify game name is unique
         global day_counter
         global daytime
         global current_game_name
+        global role_for_valid_voters
+        role_for_valid_voters = role
         day_counter = 0
         if len(valid_votes) != 0:
             valid_votes = []
-        for member in role.members:
-            # if a player doesn't have a nickname, pretend their username is their nickname by default
-            player_alias = {'username': member.name, 'nickname': member.name}
-            if(member.nick != None):
-                player_alias['nickname'] = member.nick
-            valid_votes.append(player_alias)
-        if game_name_exists(game_name):
+        evaluate_valid_voters()
+        if does_game_name_exist(game_name):
             await ctx.send('That game name already exists.')
             return
         current_game_name = game_name
@@ -66,8 +64,8 @@ async def start(ctx, game_name, role: discord.Role): #verify game name is unique
 async def vote(ctx, player:discord.Member):
     global current_game_name
     if daytime:
-        voter_is_valid = isMemberInVotingPool(ctx.author)
-        candidate_is_valid = isMemberInVotingPool(player)
+        voter_is_valid = is_member_in_voting_pool(ctx.author)
+        candidate_is_valid = is_member_in_voting_pool(player)
         if voter_is_valid and candidate_is_valid:
             item = vote_table.put_item(
                 Item={
@@ -95,8 +93,10 @@ async def start_day(ctx):
     global day_counter
     global daytime
     global game_started
+    global role_for_valid_voters
     success = increment_and_record_day()
     if success:
+        evaluate_valid_voters()
         await ctx.send(":sunny: Day " + str(day_counter)+ " has been started. Good luck!")
     else:
         if daytime:
@@ -127,10 +127,29 @@ async def end_day(ctx):
         print(json.dumps(item, indent=4))
         daytime = False
         tally = tally_votes(day_counter)
-        tally_array = [entry['name']+': '+str(entry['count']) for entry in tally] #need to double check
-        await ctx.send(':full_moon: Day ' + str(day_counter) + ' has ended. Below are the tallied votes.\n```'+'\n'.join(tally_array)+'```')
-    
-def game_name_exists(game_name):
+        tally_array = [entry['name']+': '+','.join(entry['voters'])+' ('+str(entry['count']) +')' for entry in tally]
+        if len(tally_array) != 0:
+            await ctx.send(
+                ':full_moon: Day ' + str(day_counter) + ' has ended. Below are the tallied votes.\n```' + '\n'.join(
+                    tally_array) + '```')
+        else:
+            await ctx.send('No one voted today. :man_shrugging:')
+
+
+def evaluate_valid_voters():
+    global role_for_valid_voters
+    global valid_votes
+    valid_votes = []
+    for member in role_for_valid_voters.members:
+        # if a player doesn't have a nickname, pretend their username is their nickname by default
+        player_alias = {'username': member.name, 'nickname': member.name}
+        if (member.nick != None):
+            player_alias['nickname'] = member.nick
+        valid_votes.append(player_alias)
+
+    # print(str(valid_votes))
+
+def does_game_name_exist(game_name):
     game_name_exists_query = day_table.query(
         KeyConditionExpression=Key('GameName').eq(game_name)
     )
@@ -159,13 +178,51 @@ def increment_and_record_day():
         return False
 
 def tally_votes(day):
-    vote_count = [
-        {'name': 'Forge', 'count': 3}
-    ]
-    return vote_count
+    global current_game_name
+    global daytime
+    day_start_ts = -1
+    end_ts = -1 # may be end of day, may be now... If night, then end of day ts. If day, now.
+    query_get_timestamps = day_table.query(
+        KeyConditionExpression=Key('GameName').eq(current_game_name)
+    )
+    if daytime and day==day_counter:
+        for i in query_get_timestamps['Items']:
+            if 'logDay' in i and i['logDay'] == day:
+                if i['dayStart']:
+                    day_start_ts = i['Timestamp']
+                    continue
+        end_ts = datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)")
+    else:
+        for i in query_get_timestamps['Items']:
+            if 'logDay' in i and i['logDay'] == day:
+                if i['dayStart']:
+                    day_start_ts = i['Timestamp']
+                else:
+                    end_ts = i['Timestamp']
+            if day_start_ts != '-1' and end_ts != '-1':
+                continue
+
+    votes_query = vote_table.query(
+        KeyConditionExpression=Key('GameName').eq(current_game_name) & Key('Timestamp').between(day_start_ts, end_ts),
+        ScanIndexForward=False
+    )
+
+    vote_tally = {}
+    voters_accounted = []
+    for i in votes_query['Items']:
+        if 'VoterPlayer' in i:
+            if i['VoterPlayer'] not in voters_accounted:  # only count the vote if it's the latest one
+                if i['VotedPlayer'] not in vote_tally:  # add the voted player if the key doesn't exist yet
+                    vote_tally[i['VotedPlayer']] = []
+                vote_tally[i['VotedPlayer']].append(i['VoterPlayer'])  # add the player who voted for them to the dictionary
+                voters_accounted.append(i['VoterPlayer'])
+
+    vote_generator = [{'name': i, 'count':len(vote_tally[i]), 'voters':vote_tally[i]} for i in vote_tally]
+
+    return vote_generator
         
 
-def isMemberInVotingPool(player:discord.Member):
+def is_member_in_voting_pool(player:discord.Member):
     if player is None:
         print("bad player")
     player_username = player.name
